bc8f5db02e447109e1582550806759d3f047b823
Rustam N <charlsetarleton@yandex.ru>
2023-09-23 20:53:34 +0300

    Test

--- ewm-service/src/test/java/ru/practicum/exploreWithMe/adminPackage/service/AdminCategoryServiceTest.java
+++ ewm-service/src/test/java/ru/practicum/exploreWithMe/adminPackage/service/AdminCategoryServiceTest.java
@@ -11,8 +11,6 @@ import ru.practicum.exploreWithMe.commonFiles.category.dto.NewCategoryDto;
 import ru.practicum.exploreWithMe.commonFiles.category.model.Category;
 import ru.practicum.exploreWithMe.commonFiles.category.repository.CategoryRepository;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
