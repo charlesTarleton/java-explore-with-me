0b4e286411654f3a76def9db41f1c71062b37e18
Rustam N <charlsetarleton@yandex.ru>
2023-10-02 12:42:47 +0300

    Вторая итерация третьего этапа диплома. Примечание: если я правильно понял, лишняя табуляция была из-за сбившейся настройки Continuation indent, которая была равна 8, хотя у меня в IDEA визуально отмечались нужные 4. Я поправил другие участки кода, в которых обнаружил эту ошибку, включая весь класс PrivateCommentServiceImpl

--- ewm-service/src/main/java/ru/practicum/exploreWithMe/privatePackage/service/commentService/PrivateCommentServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/exploreWithMe/privatePackage/service/commentService/PrivateCommentServiceImpl.java
@@ -34,90 +34,90 @@ import static ru.practicum.exploreWithMe.commonFiles.utils.ConstantaClass.Privat
 @RequiredArgsConstructor
 @Transactional
 public class PrivateCommentServiceImpl implements PrivateCommentService {
-	private final CommentRepository commentRepository;
-	private final UserRepository userRepository;
-	private final EventRepository eventRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
 
-	@Transactional(readOnly = true)
-	public List<CommentDto> getUserComments(Long userId, Integer from, Integer size) {
-		log.info(COMMENT_SERVICE_LOG, "получение всех комментариев пользователя с id: ", userId);
-		checkUserIsExist(userId);
-		Pageable pageable = new ExploreWithMePageable(from, size, Sort.by("id").ascending());
-		return commentRepository.getUserComments(userId, pageable).stream()
-			.map(CommentMapper::toDto)
-			.collect(Collectors.toList());
-	}
+    @Transactional(readOnly = true)
+    public List<CommentDto> getUserComments(Long userId, Integer from, Integer size) {
+        log.info(COMMENT_SERVICE_LOG, "получение всех комментариев пользователя с id: ", userId);
+        checkUserIsExist(userId);
+        Pageable pageable = new ExploreWithMePageable(from, size, Sort.by("id").ascending());
+        return commentRepository.getUserComments(userId, pageable).stream()
+            .map(CommentMapper::toDto)
+            .collect(Collectors.toList());
+    }
 
-	public CommentDto addComment(Long userId, NewCommentDto commentDto) {
-		log.info(COMMENT_SERVICE_LOG, "добавление комментария ", commentDto);
-		User user = checkUserIsExist(userId);
-		Event event = checkEventIsExist(commentDto.getEventId());
-		checkEventIsPublished(event);
-		checkUserIsNotInitiator(userId, event.getInitiator().getId());
-		return CommentMapper.toDto(commentRepository.save(CommentMapper.toComment(commentDto, user, event)));
-	}
+    public CommentDto addComment(Long userId, NewCommentDto commentDto) {
+        log.info(COMMENT_SERVICE_LOG, "добавление комментария ", commentDto);
+        User user = checkUserIsExist(userId);
+        Event event = checkEventIsExist(commentDto.getEventId());
+        checkEventIsPublished(event);
+        checkUserIsNotInitiator(userId, event.getInitiator().getId());
+        return CommentMapper.toDto(commentRepository.save(CommentMapper.toComment(commentDto, user, event)));
+    }
 
-	@Transactional(readOnly = true)
-	public CommentDto getComment(Long userId, Long commentId) {
-		log.info(COMMENT_SERVICE_LOG, "получение комментария по id: ", commentId);
-		checkUserIsExist(userId);
-		return CommentMapper.toDto(checkCommentIsExist(commentId));
-	}
+    @Transactional(readOnly = true)
+    public CommentDto getComment(Long userId, Long commentId) {
+        log.info(COMMENT_SERVICE_LOG, "получение комментария по id: ", commentId);
+        checkUserIsExist(userId);
+        return CommentMapper.toDto(checkCommentIsExist(commentId));
+    }
 
-	public CommentDto updateComment(Long userId, Long commentId, UpdateCommentDto commentDto) {
-		log.info(COMMENT_SERVICE_LOG, "обновление комментария с id: ", commentId);
-		checkUserIsExist(userId);
-		Comment oldComment = checkCommentIsExist(commentId);
-		checkUserIsCommentAuthor(userId, oldComment.getAuthor().getId());
-		return CommentMapper.toDto(commentRepository.save(CommentMapper.toComment(commentDto, oldComment)));
-	}
+    public CommentDto updateComment(Long userId, Long commentId, UpdateCommentDto commentDto) {
+        log.info(COMMENT_SERVICE_LOG, "обновление комментария с id: ", commentId);
+        checkUserIsExist(userId);
+        Comment oldComment = checkCommentIsExist(commentId);
+        checkUserIsCommentAuthor(userId, oldComment.getAuthor().getId());
+        return CommentMapper.toDto(commentRepository.save(CommentMapper.toComment(commentDto, oldComment)));
+    }
 
-	public void deleteComment(Long userId, Long commentId) {
-		log.info(COMMENT_SERVICE_LOG, "удаление комментария с id: ", commentId);
-		checkUserIsExist(userId);
-		Long authorId = checkCommentIsExist(commentId).getAuthor().getId();
-		checkUserIsCommentAuthor(userId, authorId);
-		commentRepository.deleteById(commentId);
-	}
+    public void deleteComment(Long userId, Long commentId) {
+        log.info(COMMENT_SERVICE_LOG, "удаление комментария с id: ", commentId);
+        checkUserIsExist(userId);
+        Long authorId = checkCommentIsExist(commentId).getAuthor().getId();
+        checkUserIsCommentAuthor(userId, authorId);
+        commentRepository.deleteById(commentId);
+    }
 
-	private User checkUserIsExist(Long userId) {
-		log.info("Начата процедура проверки наличия пользователя с id: {}", userId);
-		return userRepository.findById(userId)
-			.orElseThrow(() -> new UserExistException("Указанный пользователь не найден"));
-	}
+    private User checkUserIsExist(Long userId) {
+        log.info("Начата процедура проверки наличия пользователя с id: {}", userId);
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new UserExistException("Указанный пользователь не найден"));
+    }
 
-	private Event checkEventIsExist(Long eventId) {
-		log.info("Начата процедура проверки наличия события с id: {}", eventId);
-		return eventRepository.findById(eventId)
-			.orElseThrow(() -> new EventExistException("Указанное событие не найдено"));
-	}
+    private Event checkEventIsExist(Long eventId) {
+        log.info("Начата процедура проверки наличия события с id: {}", eventId);
+        return eventRepository.findById(eventId)
+            .orElseThrow(() -> new EventExistException("Указанное событие не найдено"));
+    }
 
-	private Comment checkCommentIsExist(Long commentId) {
-		log.info("Начата процедура проверки наличия комментария с id: {}", commentId);
-		return commentRepository.findById(commentId)
-			.orElseThrow(() -> new CommentExistException("Указанный комментарий не найден"));
-	}
+    private Comment checkCommentIsExist(Long commentId) {
+        log.info("Начата процедура проверки наличия комментария с id: {}", commentId);
+        return commentRepository.findById(commentId)
+            .orElseThrow(() -> new CommentExistException("Указанный комментарий не найден"));
+    }
 
-	private void checkUserIsNotInitiator(Long userId, Long initiatorId) {
-		log.info("Начата процедура проверки несоответствия автора комментария {} и инициатора события {}",
-			userId, initiatorId);
-		if (userId.equals(initiatorId)) {
-			throw new CommentAuthorException("Инициатор события не может оставлять комментарии к своему событию");
-		}
-	}
+    private void checkUserIsNotInitiator(Long userId, Long initiatorId) {
+        log.info("Начата процедура проверки несоответствия автора комментария {} и инициатора события {}",
+            userId, initiatorId);
+        if (userId.equals(initiatorId)) {
+            throw new CommentAuthorException("Инициатор события не может оставлять комментарии к своему событию");
+        }
+    }
 
-	private void checkUserIsCommentAuthor(Long userId, Long authorId) {
-		log.info("Начата процедура проверки соответствия пользователя {} и автора комментария {}",
-			userId, authorId);
-		if (!userId.equals(authorId)) {
-			throw new CommentAuthorException("Изменять или удалять комментарий может только пользователь, " +
-				"его написавший");
-		}
-	}
+    private void checkUserIsCommentAuthor(Long userId, Long authorId) {
+        log.info("Начата процедура проверки соответствия пользователя {} и автора комментария {}",
+            userId, authorId);
+        if (!userId.equals(authorId)) {
+            throw new CommentAuthorException("Изменять или удалять комментарий может только пользователь, " +
+                "его написавший");
+        }
+    }
 
-	private void checkEventIsPublished(Event event) {
-		if (!event.getStateAction().equals(EventState.PUBLISHED)) {
-			throw new EventNotPublicisedException("Указанное событие не опубликовано");
-		}
-	}
+    private void checkEventIsPublished(Event event) {
+        if (!event.getStateAction().equals(EventState.PUBLISHED)) {
+            throw new EventNotPublicisedException("Указанное событие не опубликовано");
+        }
+    }
 }
